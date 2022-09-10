0b3117455edbca7f59d8468a3e72e3e3255f15e7
balintzsedenyi <zsedenyi.balint.adam@gmail.com>
2022-09-10 10:34:53 +0200

    Bug searching and commenting

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hibás konstans név, hiányzik _ karakter
+    std::cout << '1-100 ertekek duplazasa' // ''-ok karaktert kezelnek, stringet "" közé kell fűzni, hiányzó sorvégi ;
+    for (int i = 0;) //hiányzó feltétel, és növekmény a ciklushoz
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // hiányzó feltétel a for ciklushoz
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // hiányzó sorvégi ; (értelmetlen kiírás lenne belőle, hiányzó érték adat?)
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ; szükséges a második paraméter után a for ciklusban
     {
-        atlag += b[i]
+        atlag += b[i] //hiányzó ; a sor végéről
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
